62ecaa9f296b242b2b041c889d4362c8380e36b8
Mahidur Sakib <mahidursakib@gmail.com>
2025-08-12 23:34:16 +0600

    changed in Event.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import ScheduleMeeting from './pages/ScheduleMeeting';
 import MeetingsList from './pages/MeetingsList';
 import CreatePoll from './pages/CreatePoll';
 import PollsList from './pages/PollsList';
+import EventPage from './pages/EventPage'; // ✅ Imported from the second part
 
 // Global CSS (optional, you can keep App.css from CRA)
 import './App.css';
@@ -26,14 +27,20 @@ function App() {
         {/* Register page */}
         <Route path="/register" element={<Register />} />
 
-        <Route path="/schedule-meeting" element={<ScheduleMeeting />} /> 
+        {/* Schedule Meeting */}
+        <Route path="/schedule-meeting" element={<ScheduleMeeting />} />
 
-        <Route path="/meetings" element={<MeetingsList />} /> 
+        {/* Meetings List */}
+        <Route path="/meetings" element={<MeetingsList />} />
 
+        {/* Create Poll */}
         <Route path="/create-poll" element={<CreatePoll />} />
 
+        {/* Polls List */}
         <Route path="/polls" element={<PollsList />} />
 
+        {/* ✅ Event Page Route */}
+        <Route path="/events/:id" element={<EventPage />} />
       </Routes>
     </Router>
   );
